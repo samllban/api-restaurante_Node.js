1fe9d8e293dc8043b2d3f6deaa93409d9f918f08
samllban <137834390+samllban@users.noreply.github.com>
2024-12-12 14:24:48 -0300

    update da aplicação

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ const bodyParser = require("body-parser");
 const sequelize = require('./config/database');
 const pedidosRouter = require('./routes/pedidos');
 
-
 const app = express();
+
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
 //configurando o ejs
 app.set('view engine', 'ejs');
 app.set("views", "src/views");
 
 //defifindo diretorio para arquivos estáticos
-app.set(express.static('src/public'));
+app.use(express.static('src/public'));
 
 //usando as rotas de pedidos
 app.use('/pedidos', pedidosRouter);
@@ -31,4 +31,3 @@ sequelize.sync()
     })
     .catch((err) => console.error('erro ao sincronizar com o banco de dados:', err));
     
-
